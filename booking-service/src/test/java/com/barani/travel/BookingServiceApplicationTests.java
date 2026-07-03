bbc9e4d49336066b468bd39cbd45fc6f95c4a77c
Barani K <baraniec2002@gmail.com>
2026-07-03 19:22:02 +0530

    Booking service test added

--- booking-service/src/test/java/com/barani/travel/BookingServiceApplicationTests.java
+++ booking-service/src/test/java/com/barani/travel/BookingServiceApplicationTests.java
@@ -2,8 +2,12 @@ package com.barani.travel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest(
+		properties = {
+				"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+		}
+)
 
-@SpringBootTest
 class BookingServiceApplicationTests {
 
 	@Test

c725b6dcb4d775f9e7d7092928e0985df93a166a
Barani K <baraniec2002@gmail.com>
2026-07-07 10:17:55 +0530

    booking completed

--- booking-service/src/main/java/com/barani/travel/serviceImpl/EmailServiceImpl.java
+++ booking-service/src/main/java/com/barani/travel/serviceImpl/EmailServiceImpl.java
@@ -6,9 +6,11 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
+@Async
 public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender mailSender;
